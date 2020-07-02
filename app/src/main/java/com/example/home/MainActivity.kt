03e78f31b598e0e403ad7cc4473397b5736e4390
zhanysch <zhanysch@gmail.com>
2020-07-02 11:53:24 +0600

    first comit

--- app/src/main/java/com/example/home/MainActivity.kt
+++ app/src/main/java/com/example/home/MainActivity.kt
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    val trueinputname ="maks"
+    val truepass= "12345"
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,6 +43,20 @@ class MainActivity : AppCompatActivity() {
 
         login.setOnClickListener {
             val text = inputName.text.toString()
+            val passText = pass.text.toString()
+
+
+            if (text== trueinputname && passText == truepass  ) {
+
+                Toast.makeText(applicationContext, "data is correct", Toast.LENGTH_LONG ).show()
+            }
+
+            else {
+                Toast.makeText(applicationContext, "data is not correct", Toast.LENGTH_LONG ).show()
+
+            }
+
+
 
 
             val intent = Intent(applicationContext,ThirdActivity::class.java)
